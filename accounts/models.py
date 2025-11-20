4ad85e76136a3fcb07b22f34d37894db9bdde855
ericndungutse <dav.ndungutse@gmail.com>
2025-11-20 20:09:34 +0200

    feat(jwt-auth): create user model and migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
-# Create your models here.
+class User(AbstractBaseUser):
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    # Primary login field
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True) 
+    full_name = models.CharField(max_length=150, blank=True)
+    
+    # Role choices
+    class Role(models.TextChoices):
+        STAFF = "staff", "Staff"
+        APPROVER_L1 = "approver-level-1", "Approver Level 1"
+        APPROVER_L2 = "approver-level-2", "Approver Level 2"
+        FINANCE = "finance", "Finance"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STAFF
+    )
+
+    # CRITICAL: Login is based on email
+    USERNAME_FIELD = 'email' 
+    
+    # Only prompting for full_name when creating superuser
+    REQUIRED_FIELDS = ['full_name'] 
+    
+    objects = UserManager() 
+
+    def __str__(self):
+        return self.email
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-25p80+a6zn-lv&%e3g_26pl7rl*y-uu==d=&=$9qb^ccdt)12g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,23 +33,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
 ]
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -61,8 +60,6 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -74,10 +71,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 1. Specify the engine
+        'ENGINE': 'django.db.backends.postgresql',
+        
+        # 2. Database details
+        'NAME': config('DB_NAME'),        # 'procure_to_pay_ist'
+        'USER': config('DB_USER'),        # 'postgres'
+        'PASSWORD': config('DB_PASSWORD'),# 'eric'
+        
+        # 3. Connection details
+        'HOST': config('DB_HOST', default='localhost'),  # fallback to localhost
+        'PORT': config('DB_PORT', default='5432'),       # fallback to 5432
     }
 }
 
